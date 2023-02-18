408a9d494f0dbd5bb4b9b87d8fd7c7cc04c446d7
David <davidhillton@yahoo.com.ar>
2023-02-17 21:37:06 -0300

    enable admin panel add models and search product

--- pre_entrega_tres/app_kiosco/models.py
+++ pre_entrega_tres/app_kiosco/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 class Provider(models.Model):
     brand = models.CharField(max_length=200)
+    def __str__(self):
+        return self.brand
     
 class Product(models.Model):
     name = models.CharField(max_length=200)
@@ -11,13 +13,22 @@ class Product(models.Model):
     code = models.IntegerField()
     brand = models.ForeignKey(Provider, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.name}  - {self.size} ML - {self.code}  / {self.brand.brand}'
+
 class Person(models.Model):
     name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name + ', ' + self.last_name
+
 
 class Purchase(models.Model):
     amount = models.IntegerField()
     product = models.OneToOneField(Product, on_delete=models.CASCADE)
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'Producto: {self.product.name} - Cantidad: {self.amount} - Cliente: {self.person.name}, {self.person.last_name}'
 
